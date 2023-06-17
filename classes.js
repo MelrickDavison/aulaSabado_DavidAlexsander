95b0a5d556c1562685f21a9727f5f973cc490baa
MelrickDavison <126255280+vitor-gss@users.noreply.github.com>
2023-06-17 08:32:25 -0300

    Conclusão das classes

--- classes.js
+++ classes.js
@@ -7,5 +7,38 @@ class  Cliente{
 }
 
 class Empresa{
-    
+    constructor(name, cnpj){
+        this.nome = name;
+        this.cnpj = cnpj;
+    }
 }
+
+class Pedido{
+    constructor(numPedido, endEntrega, dataPedido){
+        this.numPedido = numPedido;
+        this.endEntrega = endEntrega;
+        this.dataPedido = dataPedido;
+        this.valorPedido = 0;
+        this.listaProdutos = [];
+    }
+
+    adicionarProd(produto, quantidade){
+        for(let i = 0; i < quantidade; i++){
+        this.listaProdutos.push(produto)
+        }
+    }
+
+    valorTotal(frete){
+        for(let i = 0; this.listaProdutos.length; i++){
+           this.valorPedido += this.listaProdutos[i].preco
+        }
+        this.valorPedido += frete
+    }
+}
+
+    class Produto{
+        constructor(nome, preco){
+            this.nome = nome;
+            this.preco = preco;
+        }
+    }
